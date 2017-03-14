8172f1b218b51097e3f4b029119b00d32435f7e5
Javier Madrazo <JMadrazo@MacBook-Pro-de-Javier.local>
2017-03-14 18:14:20 -0300

    Se agrega envio de mail por PHP

---
+++ php/contacto.php
@@ -0,0 +1,37 @@
+<?php
+
+// Variables
+$nombre = $_POST['nombre'];
+$correo = $_POST['correo'];
+$mensaje = $_POST['mensaje'];
+$from = $correo;
+
+// Mail donde se reive el formulario
+$to = "contacto@mediacionesdiano.com.ar";
+
+// Asunto
+$subject = "Contacto desde Mediaciones Diano Web";
+
+// Cuerpo del mensaje
+$emailbody = "";
+$emailbody .= "Nombre: " . $nombre . "\n";
+$emailbody .= "Correo electrónico: " . $correo . "\n";
+$emailbody .= "mensaje: " . $mensaje . "\n";
+
+// UTF-8 
+$header = "";
+$header .= "From:" . $from . " \n";
+$header .= "Content-Type:text/plain;charset=utf-8";
+
+
+// Envio del correo
+$success = mail($to, $subject, $emailbody, $header);
+
+// Redireccionamiento despues de enviar el mensaje
+if ($success){
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=http://www.mediacionesdiano.com.ar\">";
+}
+else{
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=http://www.mediacionesdiano.com.ar\">";
+}
+?>
